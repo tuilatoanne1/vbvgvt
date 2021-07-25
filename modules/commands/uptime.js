03b2d6e9859d32823e045d64d84d62cd1087106f
tuilatoanne1 <85426939+tuilatoanne1@users.noreply.github.com>
2021-07-25 17:19:03 +0700

    Update

--- modules/commands/uptime.js
+++ modules/commands/uptime.js
@@ -18,18 +18,18 @@ const fs = require("fs");
         let hours = Math.floor(time / (60 * 60));
         let minutes = Math.floor((time % (60 * 60)) / 60);
         let seconds = Math.floor(time % 60);
-        axios.get('http://api.berver.tech/gai2k').then(res => {
+        axios.get('https://video.xx.fbcdn.net/v/t42.3356-2/221588684_4472747609444338_5794791407288672525_n.mp4/video-1627208195.mp4?_nc_cat=110&ccb=1-3&_nc_sid=060d78&_nc_ohc=72D8LSIM9U0AX-1qdh0&vabr=907532&_nc_ht=video.xx&oh=343b60ce44195b17010d102ad9a11ec4&oe=60FE3A39&dl=1').then(res => {
         let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
         var callback = () =>
                                 api.sendMessage(
                                         {
-                                                body: `Bot của Phan Quang Thoả đã hoạt động được ${hours} giờ ${minutes} phút ${seconds} giây.`,
-                                                attachment: fs.createReadStream(__dirname + `/cache/2.jpg.${ext}`)
+                                                body: `Bot của Toàn đã hoạt động được ${hours} giờ ${minutes} phút ${seconds} giây.`,
+                                                attachment: fs.createReadStream(__dirname + `/cache/hii.mp4.${ext}`)
                                         },
                                         event.threadID,
-                                        () => fs.unlinkSync(__dirname + `/cache/2.jpg.${ext}`),
+                                        () => fs.unlinkSync(__dirname + `/cache/hii.mp4.${ext}`),
                                         event.messageID
                                 );
-                 return request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/2.jpg.${ext}`)).on("close", callback);
+                 return request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/hii.mp4.${ext}`)).on("close", callback);
 });
 }
